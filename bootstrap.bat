8dfc817eaf8e147d657e75fb85f761780627b902
Peter Drahoš <drahosp@gmail.com>
2011-06-30 04:58:16 +0200

    Updating to LuaDist 1.2.1 and fixing bootstrap.bat thanks to Luis Lavena

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,3 +1,4 @@
+:: WARNING: This script assumes CMake 2.8.4+ and GCC/MinGW compiler is installed and available in system PATH.
 :: Copyright (C) 2011 LuaDist
 :: Redistribution and use of this file is allowed according to the terms of the MIT license.
 :: For details see the COPYRIGHT file distributed with LuaDist.
@@ -11,9 +12,9 @@ set DEPL=%CD%\_install
 
 echo WARNING: This will build LuaDist two times!
 
-cd "%REPO%" && git submodule update --init
+cd "%REPO%" && CALL git submodule update --init
 mkdir "%DEPL%\tmp\install"
-cd "%DEPL%\tmp\install" && cmake "%REPO%" -G"MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="%DEPL%" cmake --build "%DEPL%\tmp\install" --target install
-echo NOTE: If you received dependency error your build should be ok anyway. Update to CMake 2.8.4 to avoid this bug.
+cd "%DEPL%\tmp\install" && cmake "%REPO%" -G"MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="%DEPL%"
+cmake --build "%DEPL%\tmp\install" --target install
 
 endlocal
